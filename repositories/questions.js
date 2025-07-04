7a16dc39b2b226c94c11b837cd9fbfbcfcc7caec
dio067 <yousefnas06@gmail.com>
2025-07-04 20:09:09 +0400

    feat: add create function to  the questions repo

--- repositories/questions.js
+++ repositories/questions.js
@@ -1,5 +1,15 @@
 import Repository from "./repo";
 
-class Questions extends Repository {}
+class Questions extends Repository {
+	async create(attrs) {
+		attrs.id = this.randomId();
+		const records = await this.getAll();
+		records.push(attrs);
+
+		await this.writeAll(records);
+
+		return attrs;
+	}
+}
 
 export default new Questions("questions.json");
