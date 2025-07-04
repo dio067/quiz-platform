47ec49dd94d2586deb40ccf4ec218e3b456f17e1
dio067 <yousefnas06@gmail.com>
2025-07-04 19:47:48 +0400

    fix: edit create function in quiz repo

--- repositories/quizes.js
+++ repositories/quizes.js
@@ -3,6 +3,12 @@ import Repository from "./repo.js";
 class Quiz extends Repository {
 	async create(attrs) {
 		attrs.id = this.randomId();
+		const records = await this.getAll();
+		records.push(attrs);
+
+		await this.writeAll(records);
+
+		return attrs;
 	}
 }
 
