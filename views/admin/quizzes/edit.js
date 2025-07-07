f18db5c716eac451849269a47d5b2465a7d38445
dio067 <yousefnas06@gmail.com>
2025-07-07 12:38:44 +0400

    fix: update edit template

--- views/admin/quizzes/edit.js
+++ views/admin/quizzes/edit.js
@@ -2,34 +2,38 @@ import layout from "../layout.js";
 import helpers from "../../helpers.js";
 
 export default ({ quiz, errors }) => {
-	return layout`<div class="columns is-centered">
+	return layout(`
+    <div class="container">
+    <div class="columns is-centered">
         <div class="column is-half">
-          <h1 class="subtitle">Edit a Quiz</h1>
+          <h1 class="title">Edit a Quiz</h1>
 
-          <form method="POST" enctype="multipart/form-data">
+          <form method="POST">
             <div class="field">
               <label class="label">Title</label>
               <input value="${
 								quiz.title
-							}" class="input" placeholder="Title" name="title">
+							}" class="input" placeholder="Quiz Title" name="title">
               <p class="help is-danger">${helpers.getError(errors, "title")}</p>
             </div>
             
             <div class="field">
-              <label class="label">discription</label>
-              <input value="${
-								product.price
-							}" class="input" placeholder="discription" name="discription">
+              <label class="label">Description</label>
+              <textarea class="textarea" placeholder="Quiz Description" name="description" >
+                 ${quiz.description || ""}
+                </textarea>
               <p class="help is-danger">${helpers.getError(
 								errors,
-								"discription"
+								"description"
 							)}</p>
             </div>
-        
-            <br />
-            <button class="button is-primary">Edit</button>
+
+            <div class="field" >
+            <button class="button is-primary" type="submit">Update Quiz</button>
+            <a href="/admin/quizzes" class="button" >Cancel</a>
           </form>
         </div>
       </div>
-    `;
+      </div>
+    `);
 };
