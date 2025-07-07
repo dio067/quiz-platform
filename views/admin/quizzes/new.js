ca4ae78a9fde598b56b0e2343f5056eeccbbc312
dio067 <yousefnas06@gmail.com>
2025-07-07 17:15:47 +0400

    fix: update new quiz template

--- views/admin/quizzes/new.js
+++ views/admin/quizzes/new.js
@@ -1,36 +1,36 @@
 import helpers from "../../helpers.js";
 import layout from "./layout.js";
 
-export default ({ quizzes, errors }) => {
+export default ({ errors }) => {
 	return layout(` 
+    <div class="container">
         <div class="columns is-centered">
         <div class="column is-half">
-          <h1 class="subtitle">Create a Quiz</h1>
+          <h1 class="title">Create New Quiz</h1>
 
-          <form method="POST" enctype="multipart/form-data">
+          <form method="POST">
             <div class="field">
               <label class="label">Title</label>
-              <input class="input" placeholder="Title" name="title">
+              <input class="input" placeholder="Quiz Title" name="title">
               <p class="help is-danger">${helpers.getError(errors, "title")}</p>
             </div>
             
             <div class="field">
-              <label class="label">Discription</label>
-              <input class="input" placeholder="Discription" name="price">
+              <label class="label">Description</label>
+              <textarea class="textarea" placeholder="Quiz Discription" name="description"></textarea>
               <p class="help is-danger">${helpers.getError(
 								errors,
-								"discription"
+								"description"
 							)}</p>
             </div>
             
             <div class="field">
-              <label class="label">Image</label>            
-              <input type="file" name="image" />
+            <button class="button is-primary" type="submit">Create Quiz</button>
+            <a href="/admin/quizzes" class="button is-primary">Cancel</a>
             </div>
-            <br />
-            <button class="button is-primary">Create</button>
           </form>
         </div>
+      </div>
       </div>
     `);
 };
