c1fe82d37f68fd16a2c8ed9e6fbf3121ae2a27d9
dio067 <yousefnas06@gmail.com>
2025-07-07 20:41:22 +0400

    fix: correct spelling error questionId

--- repositories/quizes.js
+++ repositories/quizes.js
@@ -4,27 +4,24 @@ import questionsRepo from "./questions.js";
 class Quiz extends Repository {
 	async create(attrs) {
 		attrs.id = this.randomId();
-		attrs.questionIds = attrs.questoinIds || [];
-		attrs.createdAt = new Date().toISOString;
+		attrs.questionIds = attrs.questionIds || [];
+		attrs.createdAt = new Date().toISOString();
 		const records = await this.getAll();
 		records.push(attrs);
 
 		await this.writeAll(records);
-
 		return attrs;
 	}
 
 	async getQuestionsWithQuiz(quizId) {
 		const quiz = await this.getOne(quizId);
-
 		if (!quiz) {
 			throw new Error("Quiz not found");
 		}
 
 		const questions = [];
-
 		for (const questionId of quiz.questionIds || []) {
-			const question = await questionsRepo.getOne(questoinId);
+			const question = await questionsRepo.getOne(questionId);
 			if (question) {
 				questions.push(question);
 			}
@@ -32,7 +29,7 @@ class Quiz extends Repository {
 
 		return {
 			...quiz,
-			qeustions,
+			questions,
 		};
 	}
 
@@ -41,7 +38,7 @@ class Quiz extends Repository {
 		const quizzesWithQuestions = [];
 
 		for (let quiz of quizzes) {
-			quizzesWithQuestions(await this.getQuestionsWithQuiz(quiz.id));
+			quizzesWithQuestions.push(await this.getQuestionsWithQuiz(quiz.id));
 		}
 
 		return quizzesWithQuestions;
@@ -49,27 +46,22 @@ class Quiz extends Repository {
 
 	async addQuestionsToQuiz(quizId, questionId) {
 		const quiz = await this.getOne(quizId);
-
 		if (!quiz) {
 			throw new Error(`Quiz not found with id ${quizId}`);
 		}
 
-		const question = await this.getOne(questionId);
-
+		const question = await questionsRepo.getOne(questionId);
 		if (!question) {
-			throw new Error(`Question not found with  id ${questionId} `);
+			throw new Error(`Question not found with id ${questionId}`);
 		}
 
-		// Initialize questionIds if it doesn't exist
 		if (!quiz.questionIds) {
 			quiz.questionIds = [];
 		}
 
-		// Check if questionId exist, Add it if not
 		if (!quiz.questionIds.includes(questionId)) {
-			quiz.qeustionIds.push(questionId);
-
-			await this.update(quiz, { questionIds: quiz.questionIds });
+			quiz.questionIds.push(questionId);
+			await this.update(quizId, { questionIds: quiz.questionIds });
 		}
 
 		return quiz;
@@ -77,28 +69,25 @@ class Quiz extends Repository {
 
 	async removeQuestionFromQuiz(quizId, questionId) {
 		const quiz = await this.getOne(quizId);
-
 		if (!quiz) {
 			throw new Error(`Quiz with id ${quizId} not found`);
 		}
 
 		if (quiz.questionIds) {
-			quiz.questionId = quiz.questionId.filter((id) => id !== questionId);
-
-			await this.update(quizId, { questionId: quiz.questionIds });
+			quiz.questionIds = quiz.questionIds.filter((id) => id !== questionId);
+			await this.update(quizId, { questionIds: quiz.questionIds });
 		}
 
 		return quiz;
 	}
 
-	async getAvailableQuestion(quizId) {
+	async getAvailableQuestions(quizId) {
 		const quiz = await this.getOne(quizId);
-		const allQuestion = await questionsRepo.getAll();
+		const allQuestions = await questionsRepo.getAll();
+		const usedQuestions = quiz?.questionIds || [];
 
-		const usedQuestion = quiz?.questionIds || [];
-
-		return allQuestion.filter(
-			(question) => !usedQuestion.includes(question.id)
+		return allQuestions.filter(
+			(question) => !usedQuestions.includes(question.id)
 		);
 	}
 }
