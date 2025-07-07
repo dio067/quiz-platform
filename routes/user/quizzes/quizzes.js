ed254dbc18dd0d29277c6f21ccb3a99df9d94e75
dio067 <yousefnas06@gmail.com>
2025-07-07 10:26:11 +0400

    fix: add new user page to take quizzes

--- routes/user/quizzes/quizzes.js
+++ routes/user/quizzes/quizzes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import quizzesRepo from "../../../repositories/quizes.js";
 import quizzesIndexTemplate from "../../../views/user/quizzes/index.js";
+import middlewares from "../../admin/middlewares.js";
 
 const router = express.Router();
 
@@ -8,5 +9,23 @@ router.get("/user/quizzes", async (req, res) => {
 	const quizzes = await quizzesRepo.getAllWithQuestions();
 	res.send(quizzesIndexTemplate({ quizzes }));
 });
+
+router.get(
+	"/user/quizzes/:id/take",
+	middlewares.requireAuth,
+	async (req, res) => {
+		const quiz = await quizzesRepo.getQuestionsWithQuiz(req.params.id);
+
+		if (!quiz) {
+			return res.send("Quiz not found");
+		}
+
+		if (!quiz.questions || quiz.questions.length === 0) {
+			return res.send("Quiz has no questions");
+		}
+
+		res.send(quizTakeTemplate({ quiz }));
+	}
+);
 
 export default router;
