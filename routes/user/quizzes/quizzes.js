4b1c6afd7ca43fa2f86c479a7c7a30dc2d69fd27
dio067 <yousefnas06@gmail.com>
2025-07-07 11:05:59 +0400

    feat: add new user page to submit questions and quiz and show results

--- routes/user/quizzes/quizzes.js
+++ routes/user/quizzes/quizzes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import quizzesRepo from "../../../repositories/quizes.js";
 import quizzesIndexTemplate from "../../../views/user/quizzes/index.js";
+import quizTakeTemplate from "../../../views/user/quizzes/take.js";
 import middlewares from "../../admin/middlewares.js";
 
 const router = express.Router();
@@ -25,6 +26,49 @@ router.get(
 		}
 
 		res.send(quizTakeTemplate({ quiz }));
+	}
+);
+router.post(
+	"/user/quizzes/:id/submit",
+	middlewares.requireAuth,
+	async (req, res) => {
+		const quiz = await quizzesRepo.getQuestionsWithQuiz(req.params.id);
+		if (!quiz) {
+			return res.send("Quiz not found");
+		}
+
+		const answers = req.body.answers || {};
+
+		let score = 0;
+
+		const totalQuestions = quiz.question.length;
+
+		const results = totalQuestions.map((question, index) => {
+			const userAnswer = parseInt(answers[question.id]);
+			const isCorrect = userAnswer === question.correctAnswer;
+
+			if (isCorrect) score++;
+
+			return {
+				question: question.questionText,
+				options: question.options,
+				userAnswer,
+				correctAnswer: question.correctAnswer,
+				isCorrect,
+			};
+		});
+
+		const percentage = Math.round((score / totalQuestions) * 100);
+
+		res.send(
+			quizResultTemplate({
+				quiz,
+				result,
+				score,
+				totalQuestions,
+				percentage,
+			})
+		);
 	}
 );
 
