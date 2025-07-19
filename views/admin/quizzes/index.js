3ba3087a87496f7554b3bebec6871e8f6d1bad12
dio067 <yousefnas06@gmail.com>
2025-07-19 18:30:33 +0400

    fix: edit structure and classes names

--- views/admin/quizzes/index.js
+++ views/admin/quizzes/index.js
@@ -1,36 +1,36 @@
-import layout from "../layout.js";
+import layout from '../layout.js';
 
 export default ({ quizzes }) => {
-	const renderedQuizzes = quizzes
-		.map((quiz) => {
-			return `
+    const renderedQuizzes = quizzes
+        .map((quiz) => {
+            return `
 				<tr>
 					<td>${quiz.title}</td>
-					<td>${quiz.description || "No description"}</td>
+					<td>${quiz.description || 'No description'}</td>
 					<td>${quiz.questionIds ? quiz.questionIds.length : 0}</td>
 					<td>
 						<a href="/admin/quizzes/${
-							quiz.id
-						}/edit" class="button is-small is-info">Edit</a>
+                            quiz.id
+                        }/edit" class="button is-small is-info">Edit</a>
 					</td>
 					<td>
 						<a href="/admin/quizzes/${
-							quiz.id
-						}/questions" class="button is-small is-primary">Questions</a>
+                            quiz.id
+                        }/questions" class="button is-small is-primary">Questions</a>
 					</td>
 					<td>
 						<form action="/admin/quizzes/${
-							quiz.id
-						}/delete" method="POST" style="display: inline;">
+                            quiz.id
+                        }/delete" method="POST" style="display: inline;">
 							<button class="button is-small is-danger" type="submit" onclick="return confirm('Are you sure?')">Delete</button>
 						</form>
 					</td>
 				</tr>
 			`;
-		})
-		.join("");
+        })
+        .join('');
 
-	return layout(`
+    return layout(`
 		<div class="container">
 			<div class="level">
 				<div class="level-left">
